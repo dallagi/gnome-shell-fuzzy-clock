f41fdd193d5722dfd2eb37e3d3252fd56b1d78df
Philipp Kiemle <philipp.kiemle@gmail.com>
2020-11-20 19:05:31 +0100

    Print error message when install fails

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,5 @@
 
 npm run pack && \
 gnome-extensions install Fuzzy_Clock@dallagi.shell-extension.zip --force && \
-echo "Now restart the shell (Press 'Alt' + 'F2', then 'r')."
+echo "Now restart the shell (Press 'Alt' + 'F2', then 'r')." || echo "ERROR: Failed to run 'scripts/install.sh'."
+# Above error message gets shown if one of the three commands linked with '&&' fails.
