e8ddd737f02d6430ee4021e240c617b0f2ad236a
Philipp Kiemle <philipp.kiemle@gmail.com>
2020-11-20 19:52:35 +0100

    Add fluff text and error code if install fails

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,5 @@
 
 npm run pack && \
 gnome-extensions install Fuzzy_Clock@dallagi.shell-extension.zip --force && \
-echo "Now restart the shell (Press 'Alt' + 'F2', then 'r')." || echo "ERROR: Failed to run 'scripts/install.sh'."
-# Above error message gets shown if one of the three commands linked with '&&' fails.
+# The error message gets only shown if one of the three commands linked with '&&' fails.
+echo "Install successful! Now restart the shell (Press 'Alt' + 'F2', then 'r')." || echo "ERROR: Failed to run 'scripts/install.sh'."; exit 1
