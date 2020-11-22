1f9c136bd2fd88b57301cc3fa1fb29de0497b1b2
Philipp Kiemle <philipp.kiemle@gmail.com>
2020-11-22 17:16:30 +0100

    Fix error catching
    Now returns 0 when successful

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,4 @@
 
 npm run pack && \
 gnome-extensions install Fuzzy_Clock@dallagi.shell-extension.zip --force && \
-echo "Install successful! Now restart the shell (Press 'Alt' + 'F2', then 'r')." || echo "ERROR: Failed to run 'scripts/install.sh'."; exit 1
+echo "Install successful! Now restart the shell (Press 'Alt' + 'F2', then 'r')." || { echo "ERROR: Failed to run 'scripts/install.sh'."; exit 1; }
