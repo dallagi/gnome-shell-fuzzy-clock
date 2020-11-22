7e802183673fadbc0d6f5225c462be2565c5f307
Philipp Kiemle <philipp.kiemle@gmail.com>
2020-11-22 17:12:58 +0100

    Abbreviate comments

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Script to pack and install the extension locally, mostly for testing purposes.
+# Pack and install the extension locally.
 
 npm run pack && \
 gnome-extensions install Fuzzy_Clock@dallagi.shell-extension.zip --force && \
-# The error message gets only shown if one of the three commands linked with '&&' fails.
 echo "Install successful! Now restart the shell (Press 'Alt' + 'F2', then 'r')." || echo "ERROR: Failed to run 'scripts/install.sh'."; exit 1
