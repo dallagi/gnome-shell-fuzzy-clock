6646c1e989a4e9341b305370c1ceb500081c53dd
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-11-22 20:45:28 +0100

    Fix build command in install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,6 @@
 
 # Pack and install the extension locally.
 
-npm run pack && \
+npm run build && \
 gnome-extensions install Fuzzy_Clock@dallagi.shell-extension.zip --force && \
 echo "Install successful! Now restart the shell (Press 'Alt' + 'F2', then 'r')." || { echo "ERROR: Failed to run 'scripts/install.sh'."; exit 1; }
